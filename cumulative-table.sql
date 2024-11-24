b6b7950583f9105d03e2211fd08ae95d91d1b741
kavindushamal <64688853+dilukshashamal@users.noreply.github.com>
2024-11-25 02:53:51 +0530

    Update cumulative-table.sql

--- cumulative-table.sql
+++ cumulative-table.sql
@@ -1,48 +1,87 @@
-INSERT INTO nba_players
+-- SELECT * FROM player_seasons;
 
--- First read in yesterday from the cumulative table
+-- CREATE TYPE seasons_stats AS(
+-- 						season INTEGER,
+-- 						gp INTEGER,
+-- 						pts REAL,
+-- 						reb REAL,
+-- 						ast REAL
+-- 						)
+
+-- CREATE TABLE players (
+-- 	player_name TEXT,
+-- 	height TEXT,
+-- 	college TEXT,
+-- 	country TEXT,
+-- 	draft_year TEXT,
+-- 	draft_round TEXT,
+-- 	draft_number TEXT,
+-- 	season_stats seasons_stats[],
+-- 	current_season INTEGER,
+-- 	PRIMARY KEY(player_name, current_season)
+-- )
+
+INSERT INTO players
 WITH yesterday AS (
-    SELECT
-        player_id,
-        player_name,
-        team_id,
-        team_name,
-        game_date,
-        SUM(points) AS points,
-        SUM(rebounds) AS rebounds,
-        SUM(assists) AS assists,
-        SUM(steals) AS steals,
-        SUM(blocks) AS blocks,
-        SUM(turnovers) AS turnovers
-    FROM nba_players
-    WHERE game_date = '2020-01-01'
-    GROUP BY 1, 2, 3, 4, 5
+ 	SELECT * FROM players
+ 	WHERE current_season = 2000
+),
+	today AS (
+	SELECT * FROM player_seasons
+		WHERE season = 2001
+	)
+
+SELECT 
+		COALESCE(t.player_name, y.player_name) AS player_name,
+		COALESCE(t.height, y.height) AS height,
+		COALESCE(t.college, y.college) AS college,
+		COALESCE(t.country, y.country) AS country,
+		COALESCE(t.draft_year, y.draft_year) AS draft_year,
+		COALESCE(t.draft_round, y.draft_round) AS draft_round,
+		COALESCE(t.draft_number, y.draft_number) AS draft_number,
+		CASE WHEN y.season_stats IS NULL 
+			THEN ARRAY[ROW(
+				t.season,
+				t.gp,
+				t.pts,
+				t.reb,
+				t.ast
+				)::seasons_stats]
+			WHEN t.season IS NOT NULL THEN y.season_stats || ARRAY[ROW(
+				t.season,
+				t.gp,
+				t.pts,
+				t.reb,
+				t.ast
+				)::seasons_stats] 
+			ELSE y.season_stats
+			END seasons_stats,
+			COALESCE(t.season, y.current_season+1) as current_season		
+	FROM today t FULL OUTER JOIN yesterday y
+		ON t.player_name = y.player_name;
+
+
+SELECT * FROM players 
+WHERE current_season = 2001
+AND player_name = 'Michael Jordan';
+
+WITH unnested AS(
+	SELECT player_name,
+		UNNEST(season_stats)::seasons_stats AS season_stats
+		FROM players 
+	WHERE current_season = 2001
 )
 
--- Then insert today's data
-INSERT INTO nba_players
+SELECT player_name,
+	(season_stats::seasons_stats).*
+FROM unnested;
 
-SELECT
-    player_id,
-    player_name,
-    team_id,
-    team_name,
-    game_date,
-    SUM(points) AS points,
-    SUM(rebounds) AS rebounds,
-    SUM(assists) AS assists,
-    SUM(steals) AS steals,
-    SUM(blocks) AS blocks,
-    SUM(turnovers) AS turnovers
-FROM nba_players
-WHERE game_date = '2020-01-02'
-GROUP BY 1, 2, 3, 4, 5
-ON CONFLICT (player_id, game_date) DO UPDATE
-SET
-    points = EXCLUDED.points,
-    rebounds = EXCLUDED.rebounds,
-    assists = EXCLUDED.assists,
-    steals = EXCLUDED.steals,
-    blocks = EXCLUDED.blocks,
-    turnovers = EXCLUDED.turnovers;
-```
+
+
+
+
+
+
+
+
+
